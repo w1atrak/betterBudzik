968cf76734e7184b8bdbe818c319b19f01bc77ba
Daniel Bielaszka <danielbielaszka@gmail.com>
2025-05-07 19:57:07 +0200

    display current time & timezone info

--- app/(tabs)/clock.tsx
+++ app/(tabs)/clock.tsx
@@ -1,12 +1,37 @@
+import { useState, useEffect } from "react";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function Clock() {
+  const [time, setTime] = useState("");
+  const [utcOffset, setUtcOffset] = useState("");
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    const updateTime = () => setTime(new Date().toLocaleTimeString(undefined, { hour12: false }));
+
+    setTimeout(() => {
+      updateTime();
+      interval = setInterval(updateTime, 1000);
+    }, 1000 - new Date().getMilliseconds())
+    updateTime();
+
+    const offset = -new Date().getTimezoneOffset() / 60;
+    setUtcOffset(offset === 0 ? "UTC" : `UTC${offset > 0 ? "+" : ""}${offset}`);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <ThemedView
       style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
     >
-      <ThemedText type="title">Clock</ThemedText>
+      <ThemedText type="title">{time}</ThemedText>
+      <ThemedText type="subtitle">
+        {Intl.DateTimeFormat().resolvedOptions().timeZone} ({utcOffset})
+      </ThemedText>
     </ThemedView>
   );
 }
